043acebb8e5734438c928cab316a1688bf7fda32
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-05 16:27:48 +0530

    positive.C
    positive or negative

--- positive.C
+++ positive.C
@@ -5,9 +5,9 @@ int num;
 printf("Enter a number: \n");
 scanf("%d", &num);
 if (num > 0)
-printf("%d is a positive number \n", num);
+printf("%d is a positive  \n", num);
 else if (num < 0)
-printf("%d is a negative number \n", num);
+printf("%d is a negative  \n", num);
 else
-printf("0 is neither positive nor negative");
+printf("0 is neither positive number nor negative");
 }
